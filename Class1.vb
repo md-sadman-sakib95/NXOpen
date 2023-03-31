f1ab4c99ac14b4ef70444b8c416fe3413b064dfa
Md Sadman Sakib <sakib6@uwindsor.ca>
2023-03-31 17:37:07 -0400

    Upload

---
+++ Class1.vb
@@ -0,0 +1,34 @@
+﻿Imports NXOpenUI
+Imports NXOpen.UF
+Imports NXOpen
+Imports NXOpen.UF.UFUi
+
+Module NXJournal
+    Sub main()
+        Dim Session = Session.GetSession
+        Dim uFSession = uFSession.GetUFSession
+
+
+        Dim selFilterFnT = New UFUi.SelInitFnT(Selectionparams)
+        Dim scope As Integer = UFConstants.UF_UI_SEL_SCOPE_WORK_PART
+        Dim intPtr As System.IntPtr.Zero
+        Dim outPut As Integer
+        Dim position As New Double
+        Dim value = uFSession.Ui.
+
+    End Sub
+
+    Function Selectionparams() As int
+
+        Dim num_triples As int = 1
+        Dim masks = New UFUi.Mask[1]
+            Dim value.object_type = UFConstants.UF_face_type
+            Dim value1.object_subtype = 0
+            Dim value2.solid_type = 0
+
+            UFSession.Ui.SetSelMask(select, UFUi.SelMaskAction.SelMaskClearAndEnableSpecific, num_triples, masks)
+        Return UFConstants.UF_UI_SEL_SUCCESS
+
+    End Function
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ClassLibrary5")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ClassLibrary5")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4d4ec165-56f8-4169-8b36-44e5ef715e35")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
